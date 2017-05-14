1bad70b435e157e816a90388a1e5bd12d33e5d55
citrusE <lillyjinny@snu.ac.kr>
2017-05-14 15:48:04 +0900

    added mobile.js

---
+++ bang/mobile.js
@@ -0,0 +1,28 @@
+function Character(){
+    //variables
+    this.characterCard;
+    this.character;
+    this.characterInfo;
+    this.JobCard;
+    this.job;
+    this.mission;
+    this.life;
+    this.playingCardList; // list of name-image pair
+    this.mountedCardList; // list of name-image pair
+    //functions
+}
+
+
+
+function CardInfo(){
+    //variables
+    this.cardImage;
+    this.cardInfo;
+    //functions
+    this.showCardInfo = showCardInfo;
+}
+
+function showCardInfo(){
+    //pop up
+}
+
