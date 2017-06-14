5dc75693a96d22eb193b992a6cf16451b2334d4c
citrusE <lillyjinny@snu.ac.kr>
2017-06-14 14:52:01 +0900

    Added some MILE protocol

--- bang/connect/shared_mile.js
+++ bang/connect/shared_mile.js
@@ -1,23 +1,25 @@
-MILE.on("gameStart", function(data, from){
+MILE.on("gameScreen", function(data, from){
     $.mobile.changePage('#sharedMain');
     var totalInfo = JSON.parse(data);
-    var numPlayer = totalInfo.length;
+    var total = totalInfo.total;
+    var numPlayer = total.length;
     for(i = 0; i < numPlayer; i ++){
         var playerN = "<div id=\"player" + i + "\">";
-        var info = totalInfo[i];
+        var info = total[i];
         var job = info.job;
         var character = info.character;
         var maxLife = info.maxLife;
         var curLife = info.curLife;
         var isDead = info.dead;
         var mounted = info.mounted;
+        var inHand = info.inHand;
 
         var charName = character.name;
         var charImage = character.image;
         var charEffect = character.effect;
         var jobName = job.name;
         var jobImage = job.image;
-        var information = "<div id='status" + i + "' <img src='" + charImage + "' width='50' heigth='100'> <img src='" + jobImage + "'width='50' height='100'> <p>" + charName + "\nEFFECT: " + charEffect + "\nLIFE(cur/max): " + curLife + "/" + maxLife + "</p> </div>";
+        var information = "<div id='status" + i + "' <img src='" + charImage + "' width='50' heigth='100'> <img src='" + jobImage + "'width='50' height='100'> <p>" + charName + "\nEFFECT: " + charEffect + "\nLIFE(cur/max): " + curLife + "/" + maxLife + "\n In Hand: " + inHand +"</p> </div>";
         playerN = playerN + information;
         if(!isDead){
             var totalImage = ""
@@ -36,3 +38,32 @@ MILE.on("gameStart", function(data, from){
     }
 });
 
+MILE.on('personalAction', function(data, from){
+    var info = JSON.parse(data);
+    var from = info.from;
+    var to = info.to;
+    var used = info.used;
+}
+MILE.on('publicAction', function(data, from){
+    var info = JSON.parse(data);
+    var who = info.who;
+    var used = info.used;
+}
+MILE.on('loseLife', function(data, from){
+    var info = JSON.parse(data);
+    var who = info.who;
+    var much = info.much;
+    var by = info.by;
+}
+MILE.on('gainLife', function(data, from){
+    var info = JSON.parse(data);
+    var who = info.who;
+    var much = info.much;
+    var by = info.by;
+}
+MILE.on('dead', function(data, from){
+    var info = JSON.parse(data);
+    var who = info.who;
+    var by = info.by;
+    var job = info.job;
+}
